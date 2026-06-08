c89078091155044d8e68f6efc16a4516e9a9a941
cesarperalta <peraltagiuliano2009@mail.com>
2026-06-08 09:45:15 -0300

    Cambio en el formulario de crear alumno

--- mvc/views/partials/table_page.php
+++ mvc/views/partials/table_page.php
@@ -79,11 +79,7 @@ require_once __DIR__ . '/../layouts/sidebar.php';
 
 <script src="<?= base_url('public/js/table-loader.js') ?>"></script>
 <script src="<?= base_url('public/js/crud.js') ?>"></script>
-<script>
-    window.BASE_URL = "<?= base_url('') ?>";
-</script>
 
-<script src="<?= base_url('public/js/huella.js') ?>"></script>
 <script src="<?= base_url('public/js/huella.js') ?>"></script>
 <script>
     cargarTabla(
